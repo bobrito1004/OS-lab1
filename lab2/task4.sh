c55e595b7a1126d244ccc98ce0c34f3d56c64077
bobrito1004 <AlexLex2001@gmail.com>
2020-10-23 13:00:59 +0300

    update

--- lab2/task4.sh
+++ lab2/task4.sh
@@ -1,17 +1,19 @@
-
 #!/bin/bash
 s=""
 for pid in $(ps -A -o pid)
 do
-	ppid=$(grep -E -h -s -i "ppid:\s(.+)" /proc/$pid/status | grep -o "[0-9]\+")
+	ppid=$(grep -E -h -s -i "PPid:\s(.+)" /proc/$pid/status | grep -o "[0-9]\+")
 	ser=$(grep -E -h -s -i "se.sum_exec_runtime" /proc/$pid/sched | grep -o " [0-9.]\+")
 	ns=$(grep -E -h -s -i "nr_switches" /proc/$pid/sched | grep -o " [0-9.]\+")
+	if [[ $ser == "" && $ns == "" ]]; then
+		continue
+	fi
 	ART=$(bc <<< "scale=10;$ser/$ns")
-	if [ $ART != "0" ] 2>/dev/null
+	if [[ $ART =~ ".[0-9]\+" ]]
 		 then ART=$"0${ART}"
 	fi
-	if ([ $ART  != "" ] && [ $PPID != "" ]) 2>/dev/null
-		then s=$"${s}ProsessID : $pid, Parent_ProcessID : $ppid, Average_Running_Time : $ART\n"
+	if ([ $ART  != "" ] && [ $PPID != "" ])
+		then s=$"${s}ProsessID: $pid, Parent_ProcessID: $ppid, Average_Running_Time: $ART\n"
 	fi
 done
-echo -e "$s" | sort -k6 > ans4
+echo -e "$s" | sort -k4 > ans4
