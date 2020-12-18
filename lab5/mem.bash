39f85af4ef4047fbd3793ef87937eb3e8bcfdc82
bobrito1004 <AlexLex2001@gmail.com>
2020-12-18 14:17:32 +0300

    update

--- lab5/mem.bash
+++ lab5/mem.bash
@@ -5,8 +5,10 @@ arr=()
 while (true);
 do
 	let "step=$step+1"
-	arr2=(1 2 3 4 5 6 7 8 9 10)
-	arr=(${arr[@]} ${arr2[@]})
+	for i in {1..10}
+	do
+		arr+=($i)
+	done
 	let "z=$step%100"
 	if [[ $z == 0 ]]
 	then
